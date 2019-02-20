889624d7d69104fbd91c8afd85bd39179d0d1b39
bloodjake <47823338+bloodjake@users.noreply.github.com>
2019-02-20 22:52:48 +0700

    Add files via upload

---
+++ otchayanniePopitkiChtotoNapisat/otchayanniePopitkiChtotoNapisat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otchayanniePopitkiChtotoNapisat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length.ToString());
+            for(int i=0; i<args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.WriteLine("Press any button to continue ");
+            Console.ReadKey();
+        }
+    }
+}
